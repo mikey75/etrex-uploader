13647f75c6c480408200fbe4a7f743f7793479a3
Michal Szwaczko <mikey@wirelabs.net>
2022-12-08 17:54:05 +0100

    cleanup in tests

--- etrex-uploader-device/src/test/java/net/wirelabs/etrex/uploader/device/RootsProviderTest.java
+++ etrex-uploader-device/src/test/java/net/wirelabs/etrex/uploader/device/RootsProviderTest.java
@@ -1,11 +1,11 @@
 
 package net.wirelabs.etrex.uploader.device;
 
-import static org.mockito.Mockito.*;
-
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
 
 
 /**
@@ -17,37 +17,31 @@ public class RootsProviderTest {
     private RootsProvider rootsProvider = Mockito.spy(new RootsProvider());
 
     @Test
-    void shouldGetLinuxRootsAndWindowsRoots() {
-
+    void shouldGetLinuxRoots() {
+        doReturn("Linux").when(rootsProvider).getOperatingSystem();
         // when
-        doReturn("Linux")
-                .doReturn("Windows")
-                .when(rootsProvider).getOperatingSystem();
-       
         rootsProvider.getRoots();
+        // then
+        verify(rootsProvider, never()).windowsRoots();
         verify(rootsProvider, times(1)).linuxRoots();
-        
+
+    }
+
+    @Test
+    void shouldGetWindowsRoots() {
+        doReturn("Windows").when(rootsProvider).getOperatingSystem();
+        // when
         rootsProvider.getRoots();
+        // then
+        verify(rootsProvider, never()).linuxRoots();
         verify(rootsProvider, times(1)).windowsRoots();
-        
-        
-    }
-    @Test
-    void shouldCallOperatingSystem() {
-        rootsProvider.getRoots();
-        verify(rootsProvider, times(1)).getOperatingSystem();
     }
 
     @Test
-    void shouldThrowExceptionWhenUnknownOS() {
-
+    void shouldThrowIllegalStateExceptionWhenUnknownOS() {
         doReturn("UNKNOWN").when(rootsProvider).getOperatingSystem();
-
-        Throwable thrown = Assertions.assertThrows(IllegalStateException.class, rootsProvider::getRoots);
-
-        Assertions.assertEquals("Unsupported operating system", thrown.getMessage());
+        assertThrows(IllegalStateException.class, rootsProvider::getRoots,"Unsupported operating system");
     }
-
 
 }
 
