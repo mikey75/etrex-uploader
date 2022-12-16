92edd1ca1b08cbb08e19c860549b8f888c124d24
Michal Szwaczko <mikey@wirelabs.net>
2022-12-16 02:11:27 +0100

    avoid throwing uncatchable npe

--- src/main/java/net/wirelabs/etrex/uploader/strava/client/RateLimitInfo.java
+++ src/main/java/net/wirelabs/etrex/uploader/strava/client/RateLimitInfo.java
@@ -18,16 +18,17 @@ public class RateLimitInfo {
     private int currentHourly;
 
     public RateLimitInfo(Map<String, List<String>> headers) {
+        if (headers.containsKey("x-ratelimit-limit") && headers.containsKey("x-ratelimit-usage")) {
+            String rateLimitAllowed = headers.get("x-ratelimit-limit").get(0);
+            String rateLimitCurrent = headers.get("x-ratelimit-usage").get(0);
 
-        String rateLimitAllowed = headers.get("x-ratelimit-limit").get(0);
-        String rateLimitCurrent = headers.get("x-ratelimit-usage").get(0);
+            StringTokenizer tokenizer = new StringTokenizer(rateLimitAllowed, ",");
+            allowedHourly = Integer.parseInt(tokenizer.nextToken());
+            allowedDaily = Integer.parseInt(tokenizer.nextToken());
 
-        StringTokenizer tokenizer = new StringTokenizer(rateLimitAllowed, ",");
-        allowedHourly = Integer.parseInt(tokenizer.nextToken());
-        allowedDaily = Integer.parseInt(tokenizer.nextToken());
-
-        tokenizer = new StringTokenizer(rateLimitCurrent, ",");
-        currentHourly = Integer.parseInt(tokenizer.nextToken());
-        currentDaily = Integer.parseInt(tokenizer.nextToken());
+            tokenizer = new StringTokenizer(rateLimitCurrent, ",");
+            currentHourly = Integer.parseInt(tokenizer.nextToken());
+            currentDaily = Integer.parseInt(tokenizer.nextToken());
+        }
     }
 }
