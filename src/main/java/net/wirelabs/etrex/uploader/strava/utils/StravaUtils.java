2cb96e364d554012a00c525e3b9447c37eeda32c
Michal Szwaczko <mikey@wirelabs.net>
2022-12-16 16:06:24 +0100

    ref interceptor to util, do not send if no headers, api usage in separate panel

--- src/main/java/net/wirelabs/etrex/uploader/strava/utils/StravaUtils.java
+++ src/main/java/net/wirelabs/etrex/uploader/strava/utils/StravaUtils.java
@@ -2,10 +2,16 @@ package net.wirelabs.etrex.uploader.strava.utils;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import net.wirelabs.etrex.uploader.common.EventType;
+import net.wirelabs.etrex.uploader.common.eventbus.EventBus;
 import net.wirelabs.etrex.uploader.strava.client.StravaException;
 import net.wirelabs.etrex.uploader.strava.model.SportType;
 
 import java.io.File;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.StringTokenizer;
 
 /*
  * Created 12/10/22 by Michał Szwaczko (mikey@wirelabs.net)
@@ -43,6 +49,32 @@ public class StravaUtils {
                 .addPart("sport_type", sportType.getValue())
                 .addPart("description", description);
 
+    }
+
+    public static final String ALLOWED_DAILY = "allowedDaily";
+    public static final String ALLOWED_15MINS = "allowed15mins";
+    public static final String CURRENT_DAILY = "currentDaily";
+    public static final String CURRENT_15MINS = "current15mins";
+
+    public static void sendRateLimitInfo(Map<String, List<String>> headers) {
+
+        if (headers.containsKey("x-ratelimit-limit") && headers.containsKey("x-ratelimit-usage")) {
+
+            HashMap<String, Integer> info = new HashMap<>();
+
+            String rateLimitAllowed = headers.get("x-ratelimit-limit").get(0);
+            String rateLimitCurrent = headers.get("x-ratelimit-usage").get(0);
+
+            StringTokenizer tokenizer = new StringTokenizer(rateLimitAllowed, ",");
+            info.put(ALLOWED_15MINS, Integer.parseInt(tokenizer.nextToken()));
+            info.put(ALLOWED_DAILY, Integer.parseInt(tokenizer.nextToken()));
+
+            tokenizer = new StringTokenizer(rateLimitCurrent, ",");
+            info.put(CURRENT_15MINS, Integer.parseInt(tokenizer.nextToken()));
+            info.put(CURRENT_DAILY, Integer.parseInt(tokenizer.nextToken()));
+
+            EventBus.publish(EventType.RATELIMIT_INFO_UPDATE, info);
+        }
     }
 }
 
---
+++ src/test/java/net/wirelabs/etrex/uploader/gui/strava/account/ApiUsagePanelTest.java
@@ -0,0 +1,47 @@
+package net.wirelabs.etrex.uploader.gui.strava.account;
+
+
+import net.wirelabs.etrex.uploader.strava.utils.StravaUtils;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.awaitility.Awaitility.await;
+import static org.mockito.Mockito.spy;
+
+/*
+ * Created 12/16/22 by Michał Szwaczko (mikey@wirelabs.net)
+ */
+class ApiUsagePanelTest {
+
+    @Test
+    void shouldDisplayApiUsage() {
+        ApiUsagePanel p = spy(new ApiUsagePanel());
+        Map<String, List<String>> headers = new HashMap<>();
+
+        headers.put("x-ratelimit-limit", List.of("100,1000"));
+        headers.put("x-ratelimit-usage", List.of("90,800"));
+
+        StravaUtils.sendRateLimitInfo(headers);
+
+
+        await().atMost(Duration.ofSeconds(2)).untilAsserted(() -> {
+                    assertThat(p.getDailyLimits().getText()).isEqualTo("(800/1000)");
+                    assertThat(p.getQuarterLimits().getText()).isEqualTo("(90/100)");
+                });
+
+        // when response does not contain ratelimit headers - the labels should stay as they were
+        headers = new HashMap<>();
+        StravaUtils.sendRateLimitInfo(headers);
+
+        await().atMost(Duration.ofSeconds(2)).untilAsserted(() -> {
+            assertThat(p.getDailyLimits().getText()).isEqualTo("(800/1000)");
+            assertThat(p.getQuarterLimits().getText()).isEqualTo("(90/100)");
+        });
+    }
+
+}
