93892cedddbabd2316834a017cb70fa04df6824e
Michał Szwaczko <mikey@wirelabs.net>
2022-12-23 23:15:33 +0100

    implement api usage warning

--- src/main/java/net/wirelabs/etrex/uploader/gui/strava/account/ApiUsagePanel.java
+++ src/main/java/net/wirelabs/etrex/uploader/gui/strava/account/ApiUsagePanel.java
@@ -3,28 +3,38 @@ package net.wirelabs.etrex.uploader.gui.strava.account;
 import lombok.Getter;
 import net.miginfocom.swing.MigLayout;
 import net.wirelabs.etrex.uploader.common.EventType;
+import net.wirelabs.etrex.uploader.common.configuration.AppConfiguration;
 import net.wirelabs.etrex.uploader.common.eventbus.Event;
 import net.wirelabs.etrex.uploader.gui.components.EventAwarePanel;
 import net.wirelabs.etrex.uploader.strava.utils.StravaUtil;
 
 
 import javax.swing.*;
+import java.awt.Color;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
-/*
+/**
  * Created 12/16/22 by Michał Szwaczko (mikey@wirelabs.net)
  */
 public class ApiUsagePanel extends EventAwarePanel {
 
-    @Getter	private final JLabel dailyLimits;
-	@Getter private final JLabel quarterLimits;
+    @Getter
+    private final JLabel dailyLimits;
+    @Getter
+    private final JLabel quarterLimits;
+    private final AppConfiguration configuration;
+    private Integer currentDaily;
+    private Integer current15min;
+    private Integer allowedDaily;
+    private Integer allowed15min;
 
-    public ApiUsagePanel() {
-		setLayout(new MigLayout("insets 0 0 0 0", "[][]", "[][][]"));
+    public ApiUsagePanel(AppConfiguration configuration) {
+        this.configuration = configuration;
+        setLayout(new MigLayout("insets 0 0 0 0", "[][]", "[][][]"));
 
-		JLabel header = new JLabel("API usage:");
+        JLabel header = new JLabel("API usage:");
         JLabel dailyLabel = new JLabel("Daily:");
         JLabel quarterLabel = new JLabel("15min:");
 
@@ -36,23 +46,52 @@ public class ApiUsagePanel extends EventAwarePanel {
         add(quarterLabel, "cell 0 2,alignx right");
         add(dailyLimits, "cell 1 1");
         add(quarterLimits, "cell 1 2");
-	}
+    }
 
-	 @Override
-	    protected void onEvent(Event evt) {
-	        if (evt.getEventType() == EventType.RATELIMIT_INFO_UPDATE) {
+    @Override
+    protected void onEvent(Event evt) {
+        if (evt.getEventType() == EventType.RATELIMIT_INFO_UPDATE) {
+            Map<String, Integer> apiInfo = (Map<String, Integer>) evt.getPayload();
+            updateApiUsageLabels(apiInfo);
+            warnIfPercentageReached();
+        }
+    }
 
-				Map<String, Integer> apiInfo = (Map<String, Integer>) evt.getPayload();
+    private void warnIfPercentageReached() {
+        double percDaily = calculatePercentage(currentDaily.doubleValue(), allowedDaily.doubleValue());
+        double perc15min = calculatePercentage(current15min.doubleValue(), allowed15min.doubleValue());
 
-	            SwingUtilities.invokeLater(() -> {
-					dailyLimits.setText("(" + apiInfo.get(StravaUtil.CURRENT_DAILY) + "/" + apiInfo.get(StravaUtil.ALLOWED_DAILY) + ")");
-	                quarterLimits.setText("(" + apiInfo.get(StravaUtil.CURRENT_15MINS) + "/" + apiInfo.get(StravaUtil.ALLOWED_15MINS) + ")");
-	            });
-	        }
-	    }
+        if (percDaily >= configuration.getApiUsageWarnPercent()) {
+            dailyLimits.setForeground(Color.RED);
+        } else {
+            dailyLimits.setForeground(getForeground());
+        }
+        if (perc15min >= configuration.getApiUsageWarnPercent()) {
+            quarterLimits.setForeground(Color.RED);
+        } else {
+            quarterLimits.setForeground(getForeground());
+        }
+    }
 
-	    @Override
-	    protected Collection<EventType> subscribeEvents() {
-	        return List.of(EventType.RATELIMIT_INFO_UPDATE);
-	    }
+    private void updateApiUsageLabels(Map<String,Integer> apiInfo) {
+
+        currentDaily = apiInfo.get(StravaUtil.CURRENT_DAILY);
+        current15min = apiInfo.get(StravaUtil.CURRENT_15MINS);
+        allowedDaily = apiInfo.get(StravaUtil.ALLOWED_DAILY);
+        allowed15min = apiInfo.get(StravaUtil.ALLOWED_15MINS);
+
+        SwingUtilities.invokeLater(() -> {
+            dailyLimits.setText("(" + currentDaily + "/" + allowedDaily + ")");
+            quarterLimits.setText("(" + current15min + "/" + allowed15min + ")");
+        });
+    }
+
+    @Override
+    protected Collection<EventType> subscribeEvents() {
+        return List.of(EventType.RATELIMIT_INFO_UPDATE);
+    }
+
+    private double calculatePercentage(double value, double total) {
+        return value * 100 / total;
+    }
 }
--- src/test/java/net/wirelabs/etrex/uploader/gui/strava/account/ApiUsagePanelTest.java
+++ src/test/java/net/wirelabs/etrex/uploader/gui/strava/account/ApiUsagePanelTest.java
@@ -1,9 +1,11 @@
 package net.wirelabs.etrex.uploader.gui.strava.account;
 
 
+import net.wirelabs.etrex.uploader.common.configuration.AppConfiguration;
 import net.wirelabs.etrex.uploader.strava.utils.StravaUtil;
 import org.junit.jupiter.api.Test;
 
+import java.awt.Color;
 import java.time.Duration;
 import java.util.HashMap;
 import java.util.List;
@@ -11,18 +13,21 @@ import java.util.Map;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.awaitility.Awaitility.await;
-import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.*;
 
-/*
+/**
  * Created 12/16/22 by Michał Szwaczko (mikey@wirelabs.net)
  */
 class ApiUsagePanelTest {
 
     @Test
     void shouldDisplayApiUsage() {
-        ApiUsagePanel p = spy(new ApiUsagePanel());
+        AppConfiguration configuration = mock(AppConfiguration.class);
+        doReturn(50).when(configuration).getApiUsageWarnPercent();
+        ApiUsagePanel p = spy(new ApiUsagePanel(configuration));
         Map<String, List<String>> headers = new HashMap<>();
 
+        // simulate over the limit
         headers.put("X-RateLimit-Limit", List.of("100,1000"));
         headers.put("X-RateLimit-Usage", List.of("90,800"));
 
@@ -30,17 +35,33 @@ class ApiUsagePanelTest {
 
 
         await().atMost(Duration.ofSeconds(2)).untilAsserted(() -> {
-                    assertThat(p.getDailyLimits().getText()).isEqualTo("(800/1000)");
-                    assertThat(p.getQuarterLimits().getText()).isEqualTo("(90/100)");
-                });
+            assertThat(p.getDailyLimits().getText()).isEqualTo("(800/1000)");
+            assertThat(p.getQuarterLimits().getText()).isEqualTo("(90/100)");
+            assertThat(p.getDailyLimits().getForeground()).isEqualTo(Color.RED);
+            assertThat(p.getQuarterLimits().getForeground()).isEqualTo(Color.RED);
+        });
 
-        // when response does not contain ratelimit headers - the labels should stay as they were
+        // simulate within limit
+        headers.put("X-RateLimit-Limit", List.of("100,1000"));
+        headers.put("X-RateLimit-Usage", List.of("20,100"));
+
+        StravaUtil.sendRateLimitInfo(headers);
+
+        await().atMost(Duration.ofSeconds(2)).untilAsserted(() -> {
+            assertThat(p.getDailyLimits().getText()).isEqualTo("(100/1000)");
+            assertThat(p.getQuarterLimits().getText()).isEqualTo("(20/100)");
+            assertThat(p.getDailyLimits().getForeground()).isEqualTo(p.getForeground());
+            assertThat(p.getQuarterLimits().getForeground()).isEqualTo(p.getForeground());
+        });
+
+        // simulate no ratelimit info
+        // when response does not contain ratelimit headers - the labels should stay as they were last
         headers = new HashMap<>();
         StravaUtil.sendRateLimitInfo(headers);
 
         await().atMost(Duration.ofSeconds(2)).untilAsserted(() -> {
-            assertThat(p.getDailyLimits().getText()).isEqualTo("(800/1000)");
-            assertThat(p.getQuarterLimits().getText()).isEqualTo("(90/100)");
+            assertThat(p.getDailyLimits().getText()).isEqualTo("(100/1000)");
+            assertThat(p.getQuarterLimits().getText()).isEqualTo("(20/100)");
         });
     }
 
