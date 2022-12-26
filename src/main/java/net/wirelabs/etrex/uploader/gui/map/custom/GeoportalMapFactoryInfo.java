ffd504c5d3bd6e0f8925a83e9a76bd9f191decda
Michał Szwaczko <mikey@wirelabs.net>
2022-12-27 00:57:00 +0100

    geoportal fixes

--- src/main/java/net/wirelabs/etrex/uploader/gui/map/custom/GeoportalMapFactoryInfo.java
+++ src/main/java/net/wirelabs/etrex/uploader/gui/map/custom/GeoportalMapFactoryInfo.java
@@ -13,12 +13,10 @@ public class GeoportalMapFactoryInfo extends TileFactoryInfo {
     private static final int TILE_SIZE = 512;
 
     // max row/col numbers - taken from service's schema xsd
-    private final int[] maxRows={14538,7269,3634,1453,726,290,145,72,29,14,7,3,1,0,0,0};
-    private final int[] maxCols={16827,8413,4206,1682,841,336,168,84,33,16,8,4,1,0,0,0};
+    private final int[] maxRows={14539,7270,3635,1454,727,291,146,73,30,15,8,4,2,1,1,1};
+    private final int[] maxCols={16828,8414,4207,1683,842,337,169,85,34,17,9,5,3,2,1,1};
 
-    public int getMaxRowsAtZoom(int zoom) {
-        return maxRows[zoom];
-    }
+
 
     private final double[] longitudeDegrees = new double[MAX_ZOOM+1];
     private final double[] longitudeRadians = new double[MAX_ZOOM+1];
@@ -28,7 +26,7 @@ public class GeoportalMapFactoryInfo extends TileFactoryInfo {
      * Default constructor
      */
     public GeoportalMapFactoryInfo() {
-        this("Geoportal - Topograficzna mapa podkładowa", "http://mapy.geoportal.gov.pl/wss/service/WMTS/guest/wmts/G2_MOBILE_500?" +
+        this("Geoportal - Topograficzna mapa podkładowa", "https://mapy.geoportal.gov.pl/wss/service/WMTS/guest/wmts/G2_MOBILE_500?" +
                 "SERVICE=WMTS&REQUEST=GetTile" +
                /* "&VERSION=1.0.0" +*/
                /* "&LAYER=G2_MOBILE_500" +
@@ -44,7 +42,7 @@ public class GeoportalMapFactoryInfo extends TileFactoryInfo {
     public GeoportalMapFactoryInfo(String name, String baseURL) {
 
         super(name,
-                1, MAX_ZOOM-2, MAX_ZOOM,
+                1, MAX_ZOOM, MAX_ZOOM,
                 TILE_SIZE, true, true,
                 baseURL,
                 "x", "y", "z");
@@ -52,11 +50,11 @@ public class GeoportalMapFactoryInfo extends TileFactoryInfo {
 
         for (int z=0; z<=MAX_ZOOM; z++) {
 
-            int xx = ((maxCols[z]+1)*TILE_SIZE)/2;
-            int yy = ((maxCols[z]+1)*TILE_SIZE)/2;
+            double xx = ((maxCols[z])*TILE_SIZE)/2d;
+            double yy = ((maxRows[z])*TILE_SIZE)/2d;
 
-            longitudeDegrees[z] = ((maxCols[z]+1)*TILE_SIZE)/360.0;
-            longitudeRadians[z] = ((maxCols[z]+1)*TILE_SIZE)/(2.0 * Math.PI);
+            longitudeDegrees[z] = ((maxCols[z])*TILE_SIZE)/360.0;
+            longitudeRadians[z] = ((maxCols[z])*TILE_SIZE)/(2.0 * Math.PI);
             mapCentersInPixels[z] = new Point2D.Double(xx, yy);
         }
 
@@ -68,7 +66,7 @@ public class GeoportalMapFactoryInfo extends TileFactoryInfo {
 
     @Override
     public int getMapWidthInTilesAtZoom(int zoom) {
-        return maxCols[zoom]+1;
+        return maxCols[zoom];
     }
 
     @Override
@@ -101,7 +99,7 @@ public class GeoportalMapFactoryInfo extends TileFactoryInfo {
 
     @Override
     public String getAttribution() {
-        return "\u00A9 geoportal.gov.pl";
+        return "\u00A9 Podkładowa Mapa Topograficzna, geoportal.gov.pl";
     }
 
     @Override
