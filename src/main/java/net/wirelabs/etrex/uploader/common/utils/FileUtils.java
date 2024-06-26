eb4ff860b58b6a21a4dde31ee364137c083f7c86
michal.szwaczko <mikey@wirelabs.net>
2024-06-26 20:42:26 +0200

    support for gpx version 1.0 - add tests and stricter check

--- src/main/java/net/wirelabs/etrex/uploader/common/utils/FileUtils.java
+++ src/main/java/net/wirelabs/etrex/uploader/common/utils/FileUtils.java
@@ -75,7 +75,7 @@ public class FileUtils {
     public static boolean isOldGpxFile(File file) {
         try {
             String content = readFileToString(file, StandardCharsets.UTF_8);
-            return content.contains("<gpx version=\"1.0\"");
+            return content.contains("<gpx version=\"1.0\"") || content.contains("xmlns=\"http://www.topografix.com/GPX/1/0\"");
         } catch (IOException e) {
             log.error("Could not read file {}", file);
             return false;
