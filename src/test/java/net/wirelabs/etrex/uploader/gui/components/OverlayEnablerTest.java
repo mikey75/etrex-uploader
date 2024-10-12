de4d489e97a06225d5db5d07d53b1326dfa1af1c
mikey <mikey@wirelabs.net>
2024-10-12 22:34:40 +0200

    [missing tests] add OverlayEnabler test

---
+++ src/test/java/net/wirelabs/etrex/uploader/gui/components/OverlayEnablerTest.java
@@ -0,0 +1,44 @@
+package net.wirelabs.etrex.uploader.gui.components;
+
+import net.wirelabs.etrex.uploader.common.configuration.AppConfiguration;
+import net.wirelabs.etrex.uploader.gui.map.RoutePainter;
+import net.wirelabs.jmaps.map.MapViewer;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class OverlayEnablerTest {
+
+    @Test
+    void shouldDisableAndEnableOverlays() {
+        // init
+        AppConfiguration appConfiguration = Mockito.spy(new AppConfiguration());
+        MapViewer mapViewer = Mockito.spy(new MapViewer());
+        RoutePainter routePainter = Mockito.spy(new RoutePainter(appConfiguration));
+        OverlayEnabler overlayEnabler = Mockito.spy(new OverlayEnabler(mapViewer,routePainter));
+
+
+        // check that component instantiated the checkbox ;)
+        assertThat(overlayEnabler.getShowOverlaysCheckbox()).isNotNull();
+
+        // set some user overlay
+        mapViewer.addUserOverlay(routePainter);
+        assertThat(mapViewer.getUserOverlays()).isNotEmpty().hasSize(1).contains(routePainter);
+
+        // emulate checkbox is false (default is true, so clicking will set it to false)
+        overlayEnabler.getShowOverlaysCheckbox().doClick();
+        assertThat(overlayEnabler.getShowOverlaysCheckbox().isSelected()).isFalse();
+
+        // so the user overlays list should be cleared
+        assertThat(mapViewer.getUserOverlays()).isEmpty();
+
+        // now click again - should be true
+        overlayEnabler.getShowOverlaysCheckbox().doClick();
+        assertThat(overlayEnabler.getShowOverlaysCheckbox().isSelected()).isTrue();
+
+        // so now the list should not be empty and contain the previous overlay
+        assertThat(mapViewer.getUserOverlays()).isNotEmpty().hasSize(1).contains(routePainter);
+    }
+
+}
