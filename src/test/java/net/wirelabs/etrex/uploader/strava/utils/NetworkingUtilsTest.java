a1958974038efe146aaf41a08bca7494bccfdf68
mikey <mikey@wirelabs.net>
2024-11-16 23:39:38 +0100

    [refactor] more

---
+++ src/test/java/net/wirelabs/etrex/uploader/strava/utils/NetworkingUtilsTest.java
@@ -0,0 +1,44 @@
+package net.wirelabs.etrex.uploader.strava.utils;
+
+import net.wirelabs.etrex.uploader.tools.BaseTest;
+import net.wirelabs.etrex.uploader.tools.FakeHttpServer;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.List;
+
+import static net.wirelabs.etrex.uploader.strava.utils.NetworkingUtils.getAllIpsForHost;
+import static net.wirelabs.etrex.uploader.strava.utils.NetworkingUtils.isHostTcpPortReachable;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class NetworkingUtilsTest extends BaseTest {
+
+    @Test
+    void getIps() throws UnknownHostException {
+        List<InetAddress> l = getAllIpsForHost("localhost");
+        assertThat(l.get(0).getHostAddress()).isEqualTo("127.0.0.1");
+    }
+
+    @Test
+    void shouldThrowWnenUnknownHost() {
+        assertThatThrownBy(() -> getAllIpsForHost("kaka.nonexistent.pl"))
+                .isInstanceOf(UnknownHostException.class);
+    }
+
+    @Test
+    void httpReachable() throws IOException {
+        FakeHttpServer f = new FakeHttpServer(12345);
+        boolean b = isHostTcpPortReachable("localhost", 12345, 1000);
+        assertThat(b).isTrue();
+    }
+
+    @Test
+    void httpUnreachable()  {
+        boolean b = isHostTcpPortReachable("localhost", 80,1000);
+        verifyLogged("localhost:80 is unreachable");
+        assertThat(b).isFalse();
+    }
+}
