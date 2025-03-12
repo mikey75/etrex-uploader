aea38e8bf2a310a8a0756decdd13a05183a2e680
mikey <mikey@wirelabs.net>
2025-03-12 09:53:40 +0100

    [Issue #113] cr fixes

--- src/main/java/net/wirelabs/etrex/uploader/gui/components/Splash.java
+++ src/main/java/net/wirelabs/etrex/uploader/gui/components/Splash.java
@@ -11,12 +11,14 @@ public class Splash extends JFrame {
     private final SplashPanel splashContentPanel = new SplashPanel();
 
     public Splash() {
-
-        int height = splashContentPanel.backgroundImage.getHeight(this);
-        int width = splashContentPanel.backgroundImage.getWidth(this);
-
         setUndecorated(true);
-        setSize(width, height);
+
+        if (splashContentPanel.backgroundImage != null) {
+            setSize(splashContentPanel.backgroundImage.getWidth(this), splashContentPanel.backgroundImage.getHeight(this));
+        } else {
+            // since no image present we can't get image size -> set sensible defaults
+            setSize(400,300);
+        }
         setLocationRelativeTo(null);
         setContentPane(splashContentPanel);
         setVisible(true);
@@ -26,21 +28,21 @@ public class Splash extends JFrame {
 
     static class SplashPanel extends JPanel {
 
-        private transient Image backgroundImage;
+        private final URL imageLocation = getClass().getResource("/images/splash.png");
+        // if image is not found (unlikely) we do wish to continue so just get null image
+        // so that the dialog still displays, and background image can be final ;)
+        private final transient Image backgroundImage = (imageLocation != null) ? new ImageIcon(imageLocation).getImage() : new ImageIcon().getImage();
         private final transient JTextArea textArea = new JTextArea(12, 1);
 
         public SplashPanel() {
-            URL imageLocation = getClass().getResource("/images/splash.png");
 
-            if (imageLocation != null) {
                 setLayout(null); // x,y absolute layout
-                backgroundImage = new ImageIcon(imageLocation).getImage();
-            }
 
-            textArea.setFont(new Font("Arial", Font.BOLD, 11));
-            textArea.setBounds(12, 150, 350, 140);
-            textArea.setOpaque(false);
-            add(textArea);
+                textArea.setFont(new Font("Arial", Font.BOLD, 11));
+                textArea.setBounds(24, 150, 350, 140);
+                textArea.setOpaque(false);
+                add(textArea);
+
 
         }
 
@@ -49,19 +51,20 @@ public class Splash extends JFrame {
             super.paintComponent(g);
             g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this);
         }
+
+        void appendLine(String message) {
+            textArea.append(message + "\r\n");
+            textArea.update(textArea.getGraphics());
+        }
     }
 
     public void update(String message) {
-        appendMessage(message);
+        splashContentPanel.appendLine(message);
         Sleeper.sleepMillis(100);
     }
 
-    private void appendMessage(String message) {
-        splashContentPanel.textArea.append(message + "\r\n");
-        splashContentPanel.textArea.update(splashContentPanel.textArea.getGraphics());
-    }
 
-    public void close(){
+    public void close() {
         dispose();
     }
 }
