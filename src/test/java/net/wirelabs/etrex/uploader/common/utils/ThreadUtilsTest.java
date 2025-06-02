cb30c3b5db682ac71f7a87a30916b2fa83abac98
mikey <mikey@wirelabs.net>
2025-06-02 10:52:30 +0200

    [refactor] thread utils refactor and tests

--- src/test/java/net/wirelabs/etrex/uploader/common/utils/ThreadUtilsTest.java
+++ src/test/java/net/wirelabs/etrex/uploader/common/utils/ThreadUtilsTest.java
@@ -10,6 +10,7 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.CALLS_REAL_METHODS;
 
 @Slf4j
@@ -37,22 +38,73 @@ class ThreadUtilsTest extends BaseTest {
         // so we need to work on non-default ExecutorService so that other tests
         // using ThreadUtils don't run into already shutdown executor service
 
-        ExecutorService tempExecutorService = Executors.newCachedThreadPool();
+        ExecutorService testExecutorService = Executors.newCachedThreadPool();
 
         try (MockedStatic<ThreadUtils> threadUtils = Mockito.mockStatic(ThreadUtils.class, CALLS_REAL_METHODS)) {
-            threadUtils.when(ThreadUtils::getExecutorService).thenReturn(tempExecutorService);
+            threadUtils.when(ThreadUtils::getExecutorService).thenReturn(testExecutorService);
 
-            Runnable r = () -> Sleeper.sleepMillis(500);
+            Runnable r = this::longRunningTask;
+
+            // run 4 task threads - don't wait for completion
             ThreadUtils.runAsync(r);
+            ThreadUtils.runAsync(r);
+            ThreadUtils.runAsync(r);
+            ThreadUtils.runAsync(r);
+            // shutdown executor service
             ThreadUtils.shutdownExecutorService();
+            Sleeper.sleepSeconds(1);
+            verifyLoggedTimes(4, "Running runnable task in a separate thread"); // assert task is run
+            verifyLogged("Shutting down executor service"); // assert executor is going to be shut down
+            verifyLogged("Executor service shut down!"); // assert it is shut down
+            assertThat(testExecutorService.isShutdown()).isTrue();
 
-            verifyLogged("Waiting for the main ExecutorService to terminate");
-            verifyLogged("Done.");
+            // since we stopped the tasks that was executing Sleeper.sleep() to emulate long-running
+            // the sleep was interrupted surely, so check for that too
+            verifyLoggedTimes(4,"Error sleeping");
+        }
+
+
+    }
+
+    @Test
+    void shouldFindUnexecutedTasks() {
+        // in this test we really shut down executor service which is static final,
+        // so we need to work on non-default ExecutorService so that other tests
+        // using ThreadUtils don't run into already shutdown executor service
+
+        ExecutorService testExecutorService = Executors.newFixedThreadPool(1);
+        // one-thread pool so other tasks will be queued and not started when shutdown
+
+        try (MockedStatic<ThreadUtils> threadUtils = Mockito.mockStatic(ThreadUtils.class, CALLS_REAL_METHODS)) {
+            threadUtils.when(ThreadUtils::getExecutorService).thenReturn(testExecutorService);
+
+            Runnable r = this::longRunningTask;
+
+            ThreadUtils.runAsync(r);
+            ThreadUtils.runAsync(r);
+            ThreadUtils.runAsync(r);
+            ThreadUtils.runAsync(r);
+
+            // shutdown executor service
+            ThreadUtils.shutdownExecutorService();
+            Sleeper.sleepSeconds(1); // wait for logger to update its non thread safe internals
+            verifyLoggedTimes(1, "Running runnable task in a separate thread"); // assert first task is run
+            verifyLogged("Shutting down executor service"); // assert executor is going to be shut down
+            verifyLogged("Executor service shut down!"); // assert it is shut down
+            verifyLogged("Unstarted tasks: 3"); // we scheduled 4 tasks to a 1 thread pool, so 3 tasks still wait
+            assertThat(testExecutorService.isShutdown()).isTrue();
+
+            // since we stopped the task that was executing Sleeper.sleep() to emulate long-running
+            // the sleep was interrupted surely, so check for that too
+            verifyLoggedTimes(1,"Error sleeping");
 
         }
 
 
     }
 
-
+    private void longRunningTask() {
+        log.info("Running runnable task in a separate thread");
+        Sleeper.sleepSeconds(50);
+    }
 }
