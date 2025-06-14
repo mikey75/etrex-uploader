5337479c8b2b434c7660d9c31e881f4c9c5bc0e5
mikey <mikey@wirelabs.net>
2025-06-14 12:56:13 +0200

    [ref] extract combo renderer to own class, make test for LaFComboBox

---
+++ src/test/java/net/wirelabs/etrex/uploader/gui/settings/LookAndFeelComboBoxTest.java
@@ -0,0 +1,43 @@
+package net.wirelabs.etrex.uploader.gui.settings;
+
+import net.wirelabs.etrex.uploader.tools.BaseTest;
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+import java.util.stream.IntStream;
+
+import static javax.swing.UIManager.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LookAndFeelComboBoxTest extends BaseTest {
+
+    @Test
+    void shouldInitializeAndCheckLaFComboBox() {
+
+        int count;
+        int selected;
+
+        // create default look and feel combo
+        LookAndFeelComboBox lafCombo = new LookAndFeelComboBox();
+
+        // assert default configuration
+        count = lafCombo.getItemCount();
+        assertThat(count).isPositive();
+        selected = lafCombo.getSelectedIndex();
+        assertThat(selected).isZero();
+
+        // it should contain all required look and feels
+        LookAndFeelInfo[] info = getInstalledLookAndFeels();
+        assertThat(info).hasSize(count);
+        List<String> classNames = Arrays.stream(info).map(LookAndFeelInfo::getClassName).toList();
+        List<String> modelClassItems = IntStream.range(0, lafCombo.getItemCount()).mapToObj(lafCombo::getItemAt).toList();
+        assertThat(modelClassItems).containsExactlyInAnyOrderElementsOf(classNames);
+
+
+        // make it emit a selection change event
+        lafCombo.setSelectedIndex(count - 1);
+        // assert selection changed
+        assertThat(lafCombo.getSelectedIndex()).isEqualTo(count - 1);
+
+    }
+}
