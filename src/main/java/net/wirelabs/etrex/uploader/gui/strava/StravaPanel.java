9c7c4b3dd25d2fa145099eecb0b9479cebe5c8b4
mikey <mikey@wirelabs.net>
2025-06-15 14:19:21 +0200

    [ref] app conf is available from client now, reduce code

--- src/main/java/net/wirelabs/etrex/uploader/gui/strava/StravaPanel.java
+++ src/main/java/net/wirelabs/etrex/uploader/gui/strava/StravaPanel.java
@@ -1,6 +1,5 @@
 package net.wirelabs.etrex.uploader.gui.strava;
 
-import net.wirelabs.etrex.uploader.common.configuration.AppConfiguration;
 import net.wirelabs.etrex.uploader.gui.components.BasePanel;
 import net.wirelabs.etrex.uploader.gui.strava.account.UserAccountPanel;
 import net.wirelabs.etrex.uploader.gui.strava.activities.StravaActivitiesPanel;
@@ -10,10 +9,10 @@ import java.awt.*;
 
 public class StravaPanel extends BasePanel {
 
-    public StravaPanel(StravaClient stravaClient, AppConfiguration appConfiguration) {
+    public StravaPanel(StravaClient stravaClient) {
         super("insets 0px","[89%][11%]","[grow][grow]");
-        add(new StravaActivitiesPanel(stravaClient, appConfiguration), "cell 0 0,grow");
-        add(new UserAccountPanel(stravaClient,appConfiguration), "cell 1 0,grow");
+        add(new StravaActivitiesPanel(stravaClient), "cell 0 0,grow");
+        add(new UserAccountPanel(stravaClient), "cell 1 0,grow");
         setSize(new Dimension(800, 350));
     }
 
