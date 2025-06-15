e7692c08c5a1b78e5b1baa7fb67ef967bf8f4a9b
mikey <mikey@wirelabs.net>
2025-06-15 14:40:02 +0200

    [ref] cosmetic strava panel ref + test

--- src/main/java/net/wirelabs/etrex/uploader/gui/strava/StravaPanel.java
+++ src/main/java/net/wirelabs/etrex/uploader/gui/strava/StravaPanel.java
@@ -1,5 +1,6 @@
 package net.wirelabs.etrex.uploader.gui.strava;
 
+import lombok.Getter;
 import net.wirelabs.etrex.uploader.gui.components.BasePanel;
 import net.wirelabs.etrex.uploader.gui.strava.account.UserAccountPanel;
 import net.wirelabs.etrex.uploader.gui.strava.activities.StravaActivitiesPanel;
@@ -7,12 +8,21 @@ import net.wirelabs.etrex.uploader.strava.client.StravaClient;
 
 import java.awt.*;
 
+@Getter
 public class StravaPanel extends BasePanel {
+
+    private final StravaActivitiesPanel activitiesPanel;
+    private final UserAccountPanel userAccountPanel;
 
     public StravaPanel(StravaClient stravaClient) {
         super("insets 0px","[89%][11%]","[grow][grow]");
-        add(new StravaActivitiesPanel(stravaClient), "cell 0 0,grow");
-        add(new UserAccountPanel(stravaClient), "cell 1 0,grow");
+
+        activitiesPanel = new StravaActivitiesPanel(stravaClient);
+        userAccountPanel = new UserAccountPanel(stravaClient);
+
+        add(activitiesPanel, "cell 0 0,grow");
+        add(userAccountPanel, "cell 1 0,grow");
+
         setSize(new Dimension(800, 350));
     }
 
