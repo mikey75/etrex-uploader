cf13e55325fa78149d6da9dfde1bd20767f5ac42
mikey <mikey@wirelabs.net>
2025-06-29 10:39:25 +0200

    [ref] make polyline precission constant. explain change in comment.

--- src/test/java/net/wirelabs/etrex/uploader/common/parsers/TrackParserTest.java
+++ src/test/java/net/wirelabs/etrex/uploader/common/parsers/TrackParserTest.java
@@ -1,6 +1,7 @@
 package net.wirelabs.etrex.uploader.common.parsers;
 
 import lombok.extern.slf4j.Slf4j;
+import net.wirelabs.etrex.uploader.common.Constants;
 import net.wirelabs.etrex.uploader.tools.BaseTest;
 import net.wirelabs.jmaps.map.geo.Coordinate;
 import org.junit.jupiter.api.Test;
@@ -84,7 +85,7 @@ class TrackParserTest extends BaseTest {
     void shouldProcessPolylineTrack() {
         // this polyline is taken from one of strava activities, I checked it on strava, and it has 175 waypoints
         String polyline = "qorxHimgeCKE?E?FDCCU?m@LsANe@Fg@JQJCDK?U?RCHACFOD?ADCCDH@IBBC?EL?NBq@B^@@GQBB?CBJB?CE?HD?S@@GGD?GL@@NDMDBATMs@J\\?JEEA_@?JCFAEOQIJAHB@NMD@FDFXKRD[?e@A@CJ?LDFAEMAFGBK@`@@?ACDHEFGR?HHHAu@IYIA@HCLLFDCCA?E@LEs@GBHJAHDn@Gr@M^APOHGLGp@BZEPEBG@BO@LASCBAA?DAGBHABHDC@GM@GB@@HCE?FCG?I?DDBGIDBC@?TEG?KGJBA@?j@z@d@\\f@Pj@JH?NJXf@RXJBDHBNPNDN?@GCCG@BEL?KEIUQAI@A?MGMGEEOQY@ICIo@Be@IOQ_A[GBO`@G^@C";
-        List<Coordinate> coords = trackParser.parsePolyline(polyline, 1E5F);
+        List<Coordinate> coords = trackParser.parsePolyline(polyline);
         assertThat(coords).isNotEmpty().hasSize(175);
         // polylines have no elevation
         assertZeroElevationInResultingCoordinates(coords);
@@ -94,28 +95,20 @@ class TrackParserTest extends BaseTest {
     @Test
     void testParsePolyline() {
         // this test uses a known polyline from Google polyline dev-docs
-
         // Example encoded polyline: _p~iF~ps|U_ulLnnqC_mqNvxq`@
         // This decodes to:
         // (38.5, -120.2), (40.7, -120.95), (43.252, -126.453)
-
         String encoded = "_p~iF~ps|U_ulLnnqC_mqNvxq`@";
-        float precision = 1E5f;
 
+        List<Coordinate> coordinates = trackParser.parsePolyline(encoded);
+        assertExamplePolylineCoords(coordinates);
 
-        List<Coordinate> coordinates = trackParser.parsePolyline(encoded, precision);
-
-        assertThat(coordinates).hasSize(3);
-
-        assertCoordinate(coordinates.get(0), -120.2, 38.5);
-        assertCoordinate(coordinates.get(1), -120.95, 40.7);
-        assertCoordinate(coordinates.get(2), -126.453, 43.252);
+        // parse again with specified precision (it's the same, but for completeness)
+        coordinates = trackParser.parsePolyline(encoded,Constants.DEFAULT_POLYLINE_PRECISION);
+        assertExamplePolylineCoords(coordinates);
     }
 
-    private void assertCoordinate(Coordinate actual, double expectedLng, double expectedLat) {
-        assertThat(expectedLat).isEqualTo(actual.getLatitude());
-        assertThat(expectedLng).isEqualTo(actual.getLongitude());
-    }
+
 
     @Test
     void shouldBeEmptyOnNonTrackFile() {
@@ -156,4 +149,15 @@ class TrackParserTest extends BaseTest {
                 .doesNotContain(Double.valueOf(0));
     }
 
+    private void assertExamplePolylineCoords(List<Coordinate> coordinates) {
+        assertThat(coordinates).hasSize(3);
+        assertCoordinate(coordinates.get(0), -120.2, 38.5);
+        assertCoordinate(coordinates.get(1), -120.95, 40.7);
+        assertCoordinate(coordinates.get(2), -126.453, 43.252);
+    }
+
+    private void assertCoordinate(Coordinate actual, double expectedLng, double expectedLat) {
+        assertThat(expectedLat).isEqualTo(actual.getLatitude());
+        assertThat(expectedLng).isEqualTo(actual.getLongitude());
+    }
 }
