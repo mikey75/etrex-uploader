93b50c6112ff8048a85a81a22ec03bee57d644fd
mikey75 <mikey@wirelabs.net>
2025-08-26 13:59:23 +0200

    main refactor

--- src/main/java/net/wirelabs/etrex/uploader/EtrexUploaderRunner.java
+++ src/main/java/net/wirelabs/etrex/uploader/EtrexUploaderRunner.java
@@ -1,22 +1,17 @@
 package net.wirelabs.etrex.uploader;
 
-import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
-import net.wirelabs.etrex.uploader.utils.SystemUtils;
 import net.wirelabs.etrex.uploader.gui.EtrexUploader;
+import net.wirelabs.etrex.uploader.utils.SystemUtils;
 
 
 @Slf4j
 public class EtrexUploaderRunner {
 
-    @Getter
-    private static final SetupManager setupManager = new SetupManager();
-
     public static void main(String[] args) {
 
         try {
-            setupManager.initialize();
-            new EtrexUploader(setupManager.getAppContext());
+            new EtrexUploader();
         } catch (Exception e) {
             log.error("Fatal exception, application terminated {}", e.getMessage(), e);
             SystemUtils.systemExit(1);
