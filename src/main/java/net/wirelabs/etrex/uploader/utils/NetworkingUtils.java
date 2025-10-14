b89619204276a078a7615f2dbda035f4bf145c3b
mikey <mikey@wirelabs.net>
2025-10-14 19:34:54 +0200

    some minor fixes/enhancements

--- src/main/java/net/wirelabs/etrex/uploader/utils/NetworkingUtils.java
+++ src/main/java/net/wirelabs/etrex/uploader/utils/NetworkingUtils.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import java.io.IOException;
 import java.net.*;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
@@ -14,13 +15,20 @@ import java.util.List;
 public class NetworkingUtils {
 
     public static List<InetAddress> getAllIpsForHost(String host) throws UnknownHostException {
+        if (host == null || host.trim().isEmpty()) {
+            log.error("Host cannot be null or empty");
+            return Collections.emptyList();
+        }
         return Arrays.stream(InetAddress.getAllByName(host))
                 .filter(Inet4Address.class::isInstance)
                 .toList();
     }
 
     public static boolean isHostTcpPortReachable(String host, int port, int timeOutMillis)  {
-
+        if (port < 1 || port > 65535) {
+            log.error("Port number illegal. Must be between 1 and 65535. Was: " + port);
+            return false;
+        }
         try (Socket soc = new Socket()) {
             soc.connect(new InetSocketAddress(host, port), timeOutMillis);
         } catch (IOException e) {
