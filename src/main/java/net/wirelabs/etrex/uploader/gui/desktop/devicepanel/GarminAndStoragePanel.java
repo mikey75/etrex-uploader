c3db8e26015da50d73428cce58009666410dabf3
mikey <mikey@wirelabs.net>
2025-10-16 18:02:03 +0200

    [#210] make upload possible from local storage too

--- src/main/java/net/wirelabs/etrex/uploader/gui/desktop/devicepanel/GarminAndStoragePanel.java
+++ src/main/java/net/wirelabs/etrex/uploader/gui/desktop/devicepanel/GarminAndStoragePanel.java
@@ -13,7 +13,7 @@ public class GarminAndStoragePanel extends BasePanel {
     public GarminAndStoragePanel(UploadService uploadService, AppConfiguration appConfiguration) {
         super("insets 0","[grow]","[grow][grow]");
         add(new GarminDeviceBrowser(uploadService), "cell 0 0,grow");
-        add(new LocalStorageBrowser(appConfiguration), "cell 0 1,grow");
+        add(new LocalStorageBrowser(appConfiguration, uploadService), "cell 0 1,grow");
         setSize(new Dimension(180, 200));
     }
 
