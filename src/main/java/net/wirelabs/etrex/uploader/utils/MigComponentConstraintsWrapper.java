2c0cc57656aaae0679e6e50c07b9de9c366302e8
mikey <mikey@wirelabs.net>
2025-10-25 19:57:22 +0200

    [ref] add some common constants for recent mig changes + fix some typos in javadoc

--- src/main/java/net/wirelabs/etrex/uploader/utils/MigComponentConstraintsWrapper.java
+++ src/main/java/net/wirelabs/etrex/uploader/utils/MigComponentConstraintsWrapper.java
@@ -11,14 +11,20 @@ import net.miginfocom.layout.CC;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class MigComponentConstraintsWrapper {
 
+    // some handy/mostly used MigLayout string based constants
+    public static final String CENTER = "center";
+    public static final String TRAILING = "trailing";
+    public static final String LEFT = "left";
+    public static final String RIGHT = "right";
+
     /**
      * Most if not all component constraints start with cell (good practice - location first)
-     * so this should only method should suffice for most cases, without increasing the code size too much by overriding
+     * so this method alone should suffice for most cases, without increasing the code size too much by overriding
      * all methods from CC()
      * <p>
-     * But if you start your constraint from something different than cell (which is possible but not good practice)
-     * then your starting call should be overriden here in the same way as cell() is.
-     * It is just an override of original CC() method with new CC().method so it should be easy to find and override.
+     * But if you start your constraint from something different from 'cell' (which is possible but not good practice)
+     * then your starting call should be overridden here in the same way as cell() is.
+     * It is just an override of original CC() method with new CC().method, so it should be easy to find and override.
      */
 
     public static CC cell(int... colRowWidthHeight) {
